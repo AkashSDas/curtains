4c9c841d67f2f48bd6baafe5aa43e664560464db
AkashSDas <aakashdas368@gmail.com>
2022-12-18 00:09:35 +0530

    add snippets and pgs

---
+++ pages/movie/[id].tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function MoviePage(): JSX.Element {
+  return (
+    <Box>
+      <Heading>Movie Page</Heading>
+    </Box>
+  );
+}
---
+++ pages/series/[id].tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function SeriesPage(): JSX.Element {
+  return (
+    <Box>
+      <Heading>Series Page</Heading>
+    </Box>
+  );
+}
