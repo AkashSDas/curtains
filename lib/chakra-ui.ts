4a7add0e04a38ab25dfe33064cbef889b1f9dc9f
AkashSDas <aakashdas368@gmail.com>
2022-12-17 23:49:56 +0530

    add util and update README

--- lib/chakra-ui.ts
+++ lib/chakra-ui.ts
@@ -1,5 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
 
+export function pxToRem(px: number): string {
+  return `${px / 16}rem`;
+}
+
 export var theme = extendTheme({
   fonts: {},
   colors: {},
