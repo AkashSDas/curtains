066090508576a14aefd729cd91e5eab32d0203d4
AkashSDas <aakashdas368@gmail.com>
2022-12-18 11:40:59 +0530

    add navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import { HStack, Text } from "@chakra-ui/react";
+
+import { pxToRem } from "../lib/chakra-ui";
+
+export default function Navabr(): JSX.Element {
+  return (
+    <HStack
+      as="nav"
+      h={pxToRem(48)}
+      px={pxToRem(24)}
+      gap={pxToRem(16)}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <HStack gap={pxToRem(16)} alignItems="center">
+        <Link href="/">
+          <Text mr={pxToRem(16)} fontWeight="bold" color="red.900">
+            Curtains
+          </Text>
+        </Link>
+
+        <Link href="/movies">Movies</Link>
+        <Link href="/series">Series</Link>
+      </HStack>
+    </HStack>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
+import Navabr from "../components/navbar";
 import { theme } from "../lib/chakra-ui";
 
 import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Navabr />
       <Component {...pageProps} />
     </ChakraProvider>
   );
