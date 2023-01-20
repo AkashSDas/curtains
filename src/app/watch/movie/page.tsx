cbcbda516c05193bc78cd1c7371b644e82a4a20f
AkashSDas <aakashdas368@gmail.com>
2023-01-20 19:01:03 +0530

    add base pgs

---
+++ src/app/watch/movie/page.tsx
@@ -0,0 +1,7 @@
+export default function WatchMoviePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Watch Movie Page</h1>
+    </div>
+  );
+}
---
+++ src/app/watch/series/page.tsx
@@ -0,0 +1,7 @@
+export default function WatchSeriesPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Watch Series Page</h1>
+    </div>
+  );
+}
