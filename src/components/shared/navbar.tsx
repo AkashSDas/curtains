e8d6b885aec2780f6a178745602ca0262a2e9a38
AkashSDas <aakashdas368@gmail.com>
2023-01-21 08:36:29 +0530

    change position

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="w-full h-12 px-8 flex items-center fixed">
+    <nav className="w-full h-12 px-8 flex items-center sticky">
       <div className="font-head text-xl text-primary">CURTAINS</div>
     </nav>
   );
