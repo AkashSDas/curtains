2212a7c22e5fb855095465f920e47fcc3990fee8
AkashSDas <aakashdas368@gmail.com>
2023-01-21 11:16:43 +0530

    update video player style

--- src/components/shared/video-player.client.tsx
+++ src/components/shared/video-player.client.tsx
@@ -6,14 +6,14 @@ interface Props {
 
 export default function VideoPlayer({ src }: Props) {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-[500px]">
       {/* Mask */}
-      <div className="z-10 absolute w-full h-full flex items-end pointer-events-none">
+      <div className="z-10 absolute w-full h-[500px] flex items-end pointer-events-none">
         <div className="w-full h-full bg-gradient-to-t from-black to-transparent opacity-70"></div>
       </div>
 
       <video
-        className="w-full h-full outline-none"
+        className="w-full h-full outline-none object-cover"
         src={src}
         controls
         muted
